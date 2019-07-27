55dbf65a7e3d0510844d3c450ac224d83ebf9db3
Mostafa Abd El Fattah <mostafa.ic2@gmail.com>
2019-07-27 15:32:16 +0200

    adjust template code

--- product_alternative/models/product.py
+++ product_alternative/models/product.py
@@ -8,6 +8,26 @@ class ProductAlternativeLine(models.Model):
     product_id = fields.Many2one("product.product")
     alternative_id = fields.Many2one("product.product")
 
+class ProductTemplateInherit(models.Model):
+    _inherit = 'product.template'
+
+    alternative_ids = fields.One2many("product.alternative.line", "product_id")
+    price1 = fields.Char(string="Price 1")
+    price2 = fields.Char(string="Price 2")
+    price3 = fields.Char(string="Price 3")
+    @api.multi
+    @api.constrains('alternative_ids')
+    def _check_alternatives(self):
+        for product in self:
+            product_ids = []
+            for line in product.alternative_ids:
+                if product.id == line.alternative_id.id:
+                    raise ValidationError("You can't add the product as alternative to itself")
+                if line.alternative_id.id in product_ids:
+                    line.unlink()
+                    continue
+                product_ids.append(line.alternative_id.id)
+
 
 class ProductInherit(models.Model):
     _inherit = 'product.product'

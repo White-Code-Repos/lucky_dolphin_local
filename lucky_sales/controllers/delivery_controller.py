1b5c329fae1b503f5285fef2650730507ca34839
shivam.trivedi <shivam.trivedi>
2019-08-28 15:28:33 +0530

    [ADD]Added Delivery slip and invoice report in action of operation

---
+++ lucky_sales/controllers/delivery_controller.py
@@ -0,0 +1,42 @@
+from odoo import http,_
+from odoo.http import request
+import base64 
+import json
+import werkzeug
+
+
+
+class file_download(http.Controller):
+    @http.route(['/opening/delivery/<model("sale.order.batch"):opening>'], type='http', auth="public",website=True)
+    def opening_delivery(self, opening, **post):
+        filename="Delivery_Slip.pdf"
+        pdf = ''
+        delivery_slip_ids = []
+        if opening.order_ids:
+            ids = opening.order_ids.mapped('picking_ids').mapped('id')
+            pdf = request.env.ref('stock.action_report_delivery').sudo().render_qweb_pdf(ids)[0]
+            pdfhttpheaders = [
+                ('Content-Type', 'application/pdf'),
+                ('Content-Length', len(pdf)),('Content-Disposition', 'attachment; filename=' + filename + ';')
+            ]
+        else:
+            raise werkzeug.exceptions.NotFound("No Delivery Slip found for linked sale!") 
+        return request.make_response(pdf, headers=pdfhttpheaders)
+
+    @http.route(['/opening/invoice/<model("sale.order.batch"):opening>'], type='http', auth="public",website=True)
+    def opening_invoice(self, opening, **post):
+        filename="Invoice.pdf"
+        if opening.order_ids:
+
+            # for sale_order_id in opening.order_ids:
+            #     if sale_order_id.invoice_ids:
+            ids = opening.order_ids.mapped('invoice_ids').mapped('id')
+
+            pdf = request.env.ref('account.account_invoices').sudo().render_qweb_pdf(ids)[0]
+            pdfhttpheaders = [
+                ('Content-Type', 'application/pdf'),
+                ('Content-Length', len(pdf)),('Content-Disposition', 'attachment; filename=' + filename + ';')
+            ]
+            return request.make_response(pdf, headers=pdfhttpheaders)
+        else:
+            raise werkzeug.exceptions.NotFound("No Account Invoice found for linked sale!") 

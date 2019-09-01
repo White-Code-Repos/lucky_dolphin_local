11eba2bd8adba7b71e7400a0331373acede23b95
divya vyas <divyapanchal759@gmail.com>
2019-09-02 00:37:04 +0530

    add new module for popup notification

--- custom_lucky/models/product.py
+++ custom_lucky/models/product.py
@@ -4,18 +4,20 @@
 from odoo import models, fields, api , _
 from datetime import datetime, timedelta,date
 from odoo.exceptions import UserError, ValidationError
+from ast import literal_eval
 
 
 # Product Template ,add selection field
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
-
-    
+ 
     def _get_product_speed_state(self):
+        print (">>>>>>>>>>>>>>>self>>>>>>>",self)
         for product in self:
             today = fields.Date.today()
             dead_days = self.env['ir.config_parameter'].sudo().get_param('dead_product_days')
             slow_days = self.env['ir.config_parameter'].sudo().get_param('slow_product_days')
+            print ("********************TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT")
             before_365_date = today - timedelta(days=float(dead_days))
             before_90_date = today - timedelta(days=float(slow_days))
             convert_before_datetime = datetime.strptime(before_365_date.strftime('%Y-%m-%d'), '%Y-%m-%d')
@@ -28,33 +30,27 @@ class ProductTemplate(models.Model):
                 bet_365_to_90_move_ids = self.env['stock.move'].search([('product_id','=',product_ids[0].id),('date','>=',convert_before_datetime),('date','<=',convert_90_datetime),('state','=','done')])
                 if not stock_365_move_ids:
                     product.product_speed_state = 'dead_product'
+                    product.write({'product_speed':'dead_product'})
+                    
                 if not stock_90_move_ids and  bet_365_to_90_move_ids :
                     product.product_speed_state = 'slow_product'
+                    product.write({'product_speed':'slow_product'})
                 if stock_365_move_ids and stock_90_move_ids:
                     product.product_speed_state = 'fast_product'
-        return 
- 
-    @api.depends('market_price', 'last_purchase_price')
-    def _get_price_diff(self):
-        for product in self:
-            if product:
-               purchase_price = product.last_purchase_price
-               market_price = product.market_price
-               if purchase_price:
-                   product.price_diff = ((market_price - purchase_price) / purchase_price) * 100
-               else:
-                   product.price_diff = 0.0
-        return 
+                    product.write({'product_speed':'fast_product'})
+                print (">>>>>>>>>>>>>product.product_speed>>>>>>>>>>>.",product.product_speed )
+        return
 
-    product_speed_state = fields.Selection([('fast_product','Fast Products'),('slow_product','Slow Products'),('dead_product','Dead Products')], string='Product Speed State', compute='_get_product_speed_state',store=True, readonly=True) 
+
+    product_speed_state = fields.Selection([('fast_product','Fast Products'),('slow_product','Slow Products'),('dead_product','Dead Products')], string='Product Speed State', compute='_get_product_speed_state', readonly=True) 
     market_price = fields.Float('Market Price',default=0.0)
     last_purchase_price = fields.Float('Last Purchase Price',readonly=True)
-    min_qty = fields.Integer('Safety Stock')
+    min_qty = fields.Integer('Minimum Quantity')
     price_diff = fields.Float(compute='_get_price_diff',string='Price Difference %', store=True, readonly=True)
+    product_speed = fields.Selection([('fast_product','Fast Products'),('slow_product','Slow Products'),('dead_product','Dead Products')], string='Product Speed State', readonly=True)
 
 class ProductProduct(models.Model):
     _inherit = 'product.product'
 
     product_speed_state = fields.Selection([('fast_product','Fast Products'),('slow_product','Slow Products'),('dead_product','Dead Products')], string='Product Speed State', related="product_tmpl_id.product_speed_state", readonly=True)
-    purchase_order_ids = fields.One2many(comodel_name='purchase.order.list',inverse_name='product_product_id')
 

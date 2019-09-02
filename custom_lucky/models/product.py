c5c5ecc6459b3d7f8a43b86caefff490c3e5ee73
divya vyas <divyapanchal759@gmail.com>
2019-09-03 00:55:30 +0530

    changes done in product speed state field and sale delivery price comment

--- custom_lucky/models/product.py
+++ custom_lucky/models/product.py
@@ -17,11 +17,8 @@ class ProductTemplate(models.Model):
         print (">>>>>>>>>>>>>>>self>>>>>>>",self)
         for product in self:
             today = fields.Date.today()
-            dead_days = self.env['ir.config_parameter'].sudo().get_param('dead_product_days')
-            slow_days = self.env['ir.config_parameter'].sudo().get_param('slow_product_days')
-            print ("********************TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT")
-            before_365_date = today - timedelta(days=float(dead_days))
-            before_90_date = today - timedelta(days=float(slow_days))
+            before_365_date = today - timedelta(days=365)
+            before_90_date = today - timedelta(days=90)
             convert_before_datetime = datetime.strptime(before_365_date.strftime('%Y-%m-%d'), '%Y-%m-%d')
             convert_90_datetime =  datetime.strptime(before_90_date.strftime('%Y-%m-%d'), '%Y-%m-%d')
             today_datetime = fields.datetime.now()
@@ -32,13 +29,11 @@ class ProductTemplate(models.Model):
                 bet_365_to_90_move_ids = self.env['stock.move'].search([('product_id','=',product_ids[0].id),('date','>=',convert_before_datetime),('date','<=',convert_90_datetime),('state','=','done')])
                 if not stock_365_move_ids:
                     product.product_speed_state = 'dead_product'
-                    product.write({'product_speed':'dead_product'})
                 if not stock_90_move_ids and  bet_365_to_90_move_ids :
                     product.product_speed_state = 'slow_product'
-                    product.write({'product_speed':'slow_product'})
                 if stock_365_move_ids and stock_90_move_ids:
                     product.product_speed_state = 'fast_product'
-                    product.write({'product_speed':'fast_product'})
+                
         return
 
     @api.depends('market_price', 'last_purchase_price')
@@ -53,13 +48,12 @@ class ProductTemplate(models.Model):
                    product.price_diff = 0.0
         return 
 
-    product_speed_state = fields.Selection([('fast_product','Fast Products'),('slow_product','Slow Products'),('dead_product','Dead Products')], string='Product Speed State', compute='_get_product_speed_state', readonly=True) 
+    product_speed_state = fields.Selection([('fast_product','Fast Products'),('slow_product','Slow Products'),('dead_product','Dead Products')], string='Product Speed State', compute='_get_product_speed_state',  store=True, readonly=True) 
     market_price = fields.Float('Market Price',default=0.0)
     last_purchase_price = fields.Float('Last Purchase Price',readonly=True)
     min_qty = fields.Integer('Minimum Quantity')
     price_diff = fields.Float(compute='_get_price_diff',string='Price Difference %', store=True, readonly=True)
     #add extra product speed field for group by 
-    product_speed = fields.Selection([('fast_product','Fast Products'),('slow_product','Slow Products'),('dead_product','Dead Products')], string='Product Speed State', readonly=True)
 
 class ProductProduct(models.Model):
     _inherit = 'product.product'

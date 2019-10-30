de07f184262e949d94c5dc75acaae76cfe5e3472
zina31 <mostafa@white-code.co.uk>
2019-10-30 14:33:00 +0200

    account check repoer

---
+++ lucky_accounting/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Lucky Accounting Changes",
+
+    'summary': """
+        Lucky Accounting Changes""",
+
+    'description': """
+        Issue Checks Printing
+    """,
+
+    'author': "Zienab Abd EL Nasser",
+    'website': "",
+
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'depends':['account_check'],
+    'data': [
+        'views/issue_check.xml',
+        'reports/account_chech.xml',
+    ],
+}
---
+++ reordering_requisitions/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Sales Planning",
+
+    'summary': """
+        Passap Plainning Of Sales""",
+
+    'description': """
+        Passap Plainning Of Sales
+    """,
+
+    'author': "Zienab Abd EL Nasser",
+    'website': "",
+
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'data': [
+        'security/ceo_group.xml',
+         'security/ir.model.access.csv',
+        'views/year_plan.xml',
+        'views/month_plan.xml',
+        'views/monthly_plan_report.xml',
+        'views/yearly_plan_report.xml',
+        'reports/plan_reports.xml'
+    ],
+}

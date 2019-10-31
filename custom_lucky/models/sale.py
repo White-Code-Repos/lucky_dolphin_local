e49067468586fcaf1bd6c3e01ff9f35799b6f7db
zina31 <mostafa@white-code.co.uk>
2019-10-31 08:09:32 +0200

    Adding currency

--- custom_lucky/models/sale.py
+++ custom_lucky/models/sale.py
@@ -176,7 +176,7 @@ class SaleOrderLine(models.Model):
             date = self._context.get('date') or fields.Date.today()
             company = self.env['res.company'].browse(self._context.get('company_id')) or self.env[
                 'res.users']._get_company()
-            rate = self.env['res.currency']._get_conversion_rate(from_currency, to_currency,company,date)
+            rate = self.env['res.currency']._get_conversion_rate(from_currency, to_currency,c)
             amount = to_currency.round(from_amount * rate) if round else from_amount * rate
         return amount
 
@@ -184,15 +184,9 @@ class SaleOrderLine(models.Model):
     def action_price(self):
         if self.not_available:
             if self.currency.id == self.currency_id.id:
-                print("%%%%%%%%%%%%%5%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
-                print(self.overall_cost)
                 self.write({'purchase_price': self.overall_cost, 'price_state': 'not_available'})
             else:
-                currency_pool = self.env['res.currency']
                 cost = self.compute_purchase_price(self.currency,self.currency_id,self.overall_cost)
-                print(cost)
-                raise Warning(cost)
-                print(cost)
                 self.write({'purchase_price': cost, 'price_state': 'not_available'})
 
         if self.overall_cost:
